3b96a661177c51fe61822b2083e99649fa04c409
云烟 <107905669+yunyandegit@users.noreply.github.com>
2023-10-14 23:37:00 +0800

    Delete Projects directory

--- Projects/WordBreakSoln.py
+++
@@ -1,6 +0,0 @@
-def wordBreak(wordList, word):
-	if word == '':
-		return True
-	else:
-		wordLen = len(word)
-		return any([(word[:i] in wordList) and wordBreak(wordList, word[i:]) for i in range(1, wordLen+1)])
--- Projects/languagetranslator.py
+++
@@ -1,9 +0,0 @@
-from googletrans import Translator
-
-la=input("Enter the sentence:")
-
-t=Translator()
-
-result=t.translate(la,src='en', dest='ne')
-
-print(result)
